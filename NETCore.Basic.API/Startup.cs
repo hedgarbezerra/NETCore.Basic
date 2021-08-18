75c5825c21438e9f752bed42ab5abc94bba72c77
Hedgar Bezerra <hedgarbezerra35@outlook.com>
2021-08-17 21:03:37 -0300

    Tratamentos para Json nos controllers e interfaces de encriptação criadas

--- NETCore.Basic.API/Startup.cs
+++ NETCore.Basic.API/Startup.cs
@@ -7,10 +7,12 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NETCore.Basic.Util.Crypto;
 using NETCore.Basic.Util.Helper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace NETCore.Basic.API
@@ -27,7 +29,19 @@ namespace NETCore.Basic.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<Repository.DataContext.Context>(opt => opt.UseSqlServer(Configuration.GetConnectionString("ConnectionString")));
-            services.AddControllers();
+            services.AddControllers()
+                .AddJsonOptions(ops =>
+                {
+                    ops.JsonSerializerOptions.IgnoreNullValues = false;
+                    ops.JsonSerializerOptions.WriteIndented = true;
+                    ops.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+                    ops.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+                })
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
+            services.AddSingleton<IHashing, Hashing>();
+            services.AddSingleton<IEncryption, Encryption>();
+
             services.Configure<ConfigurationKeys>(Configuration.GetSection("ConfiguratonKeys"));
         }
 
