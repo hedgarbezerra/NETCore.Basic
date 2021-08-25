c5167632ecc45c39698d08c46defc786d4034a11
Hedgar Bezerra <hedgarbezerra35@outlook.com>
2021-08-25 11:21:00 -0300

    Auth service Token tests

--- NETCore.Basic.Tests/Services/DataServices/AuthServiceTest.cs
+++ NETCore.Basic.Tests/Services/DataServices/AuthServiceTest.cs
@@ -33,18 +33,28 @@ namespace NETCore.Basic.Tests.Services.DataServices
             var generatedToken = _authService.GenerateToken(user);
             var jwtHandler = new JwtSecurityTokenHandler();
             var jwtSecurityToken = jwtHandler.ReadJwtToken(generatedToken);
-            var ExpiresAt = jwtSecurityToken.ValidFrom.AddMinutes(15);
             var email = (string)jwtSecurityToken.Payload.GetValueOrDefault("email");
             var role = Enum.Parse(typeof(Role), (string)jwtSecurityToken.Payload.GetValueOrDefault("role"));
             var username = (string)jwtSecurityToken.Payload.GetValueOrDefault("unique_name");
 
             Assert.That(jwtHandler.CanReadToken(generatedToken));
-            Assert.That(jwtSecurityToken.Payload.ValidTo <= ExpiresAt);
-            Assert.That(jwtSecurityToken.Issuer == "NET Core API");
+            Assert.That(jwtSecurityToken.Payload.ValidTo >= DateTime.UtcNow);
             Assert.AreEqual(user.Email, email);
             Assert.AreEqual(user.Role, role);
             Assert.AreEqual(user.Username, username);
             Assert.AreEqual("NET Core API", jwtSecurityToken.Issuer);
+        }
+
+        [Test]
+        [TestCase(@"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImhlZGdhcl9wZG1AaG90bWFpbG9tIiwidW5pcXVlX25hbWUiOiJqb3NlMSIsInJvbGUiOiJBZG1pbmlzdHJhdG9yIiwibmJmIjoxNjI5ODk1ODI4LCJleHAiOjE2Mjk4OTY3MjgsImlhdCI6MTYyOTg5NTgyOCwiaXNzIjoiTkVUIENvcmUgQVBJIn0.bk8hmjQ3uHZf3v85t1OivEzyS0GhteM9bceNJ_kkZWA")]
+        [TestCase(@"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImhlZGdhcl9wZG1AaG90bWFpbG9tIiwidW5pcXVlX25hbWUiOiJqb3NlIiwicm9sZSI6IkNvbW1vbiIsIm5iZiI6MTYyOTg5NTg5MSwiZXhwIjoxNjI5ODk2NzkxLCJpYXQiOjE2Mjk4OTU4OTEsImlzcyI6Ik5FVCBDb3JlIEFQSSJ9.hr_yFu3Wrz6-AW8tT0up6RY5602KcG5mAV1iuhKUlb0")]
+        public void GenerateToken_ExpiredToken(string token)
+        {
+            var jwtHandler = new JwtSecurityTokenHandler();
+            var jwtSecurityToken = jwtHandler.ReadJwtToken(token);
+
+            Assert.That(jwtHandler.CanReadToken(token));
+            Assert.That(jwtSecurityToken.Payload.ValidTo <= DateTime.UtcNow);
         }
     }
 }
