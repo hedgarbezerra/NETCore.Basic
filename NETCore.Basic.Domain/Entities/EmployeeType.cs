92a4fde36d7618f64c07548450c98650c241ef4c
Hedgar Bezerra <hedgarbezerra35@outlook.com>
2022-04-14 21:02:03 -0300

    Exemplo de alternativa ao uso de enumeração(evitando uso de switchs para aplicar certas regras) fonte: https://lostechies.com/jimmybogard/2008/08/12/enumeration-classes/

---
+++ NETCore.Basic.Domain/Entities/EmployeeType.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NETCore.Basic.Domain.Entities
+{
+    public abstract class EmployeeType : Enumeration
+    {
+        public static readonly EmployeeType Manager
+            = new ManagerType();
+
+        protected EmployeeType() { }
+        protected EmployeeType(int value, string displayName) : base(value, displayName) { }
+
+        //Definir prop de acordo com a regra de negócio que deverá ser aplicada à cada tipo
+        public abstract decimal BonusSize { get; }
+
+        private class ManagerType : EmployeeType
+        {
+            public ManagerType() : base(0, "Manager") { }
+
+            public override decimal BonusSize
+            {
+                get { return 1000m; }
+            }
+        }
+        private class CommonType : EmployeeType
+        {
+            public CommonType() : base(0, "Common") { }
+
+            public override decimal BonusSize
+            {
+                get { return 10m; }
+            }
+        }
+    }
+}
